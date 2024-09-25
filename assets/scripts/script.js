f484adbb1ee09fdf4d27965ed3c2c85afe1f33b4
unknown <diloch111@gmail.com>
2024-09-25 02:56:05 +0100

    generate password and update in UI

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,53 +4,58 @@ let numbersEl = document.querySelector("#numbers");
 let specialCharEl = document.querySelector("#special");
 let generateBtnEl = document.querySelector("#generate");
 let passwordEl = document.querySelector("#password");
+let lower = "abcdefghijklmnopqrstuvwxyz";
+let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let nums = "1234567890";
+let specials = '!@#$%^&*()_+-=[]{}|;:",.<>?/`~';
 
 let allCharacters = "";
-let password = ""
+let password = "";
 // take action upon clicking the generate password button
 generateBtnEl.addEventListener("click", function () {
-  populateChars();
-  generatePassword();
+  if (lengthOfPassword()) {
+    populateChars();
+    generatePassword();
+    alertWarning(passwordEl, 'lightBlue')
+    passwordEl.textContent = password;
+  }
+
   clear();
-  
 
   allCharacters = "";
-  password = ''
+  password = "";
 });
 
 function generatePassword() {
-    let x = lengthOfPassword()
-    while (x > 0) {
-    let i = Math.floor(Math.random() * allCharacters.length)
-    password += allCharacters[i]
-    x -= 1
-    }
-    
+  let x = lengthOfPassword();
+  while (x > 0) {
+    let i = Math.floor(Math.random() * allCharacters.length);
+    password += allCharacters[i];
+    x -= 1;
+  }
 }
 
 // add to list of password characters based on user requirements
 function populateChars() {
-  if (lengthOfPassword()) {
-    allCharacters += "abcdefghijklmnopqrstuvwxyz";
-    if (upperCasesEl.checked === true) {
-      allCharacters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    }
-    if (numbersEl.checked === true) {
-      allCharacters += "1234567890";
-    }
-    if (specialCharEl.checked === true) {
-      allCharacters += '!@#$%^&*()_+-=[]{}|;:",.<>?/`~';
-    }
+  allCharacters += lower;
+  if (upperCasesEl.checked === true) {
+    allCharacters += upper;
+  }
+  if (numbersEl.checked === true) {
+    allCharacters += nums;
+  }
+  if (specialCharEl.checked === true) {
+    allCharacters += specials;
   }
 }
 
 // validates length of password input and return it
 function lengthOfPassword() {
   if (!passwordLengthEl.value) {
-    alertWarning(passwordEl);
+    alertWarning(passwordEl, 'red');
     passwordEl.textContent = "Enter Password Lenght!";
   } else if (passwordLengthEl.value < 8 || passwordLengthEl.value > 12) {
-    alertWarning(passwordEl);
+    alertWarning(passwordEl, 'red');
     passwordEl.textContent = "Enter Required Lenght!";
   } else {
     // reset old password before generating a new one
@@ -68,6 +73,6 @@ function clear() {
 }
 
 // change alert warning color to red
-function alertWarning(element) {
-  return (element.style.color = "red");
+function alertWarning(element, textColour) {
+  return (element.style.color = textColour);
 }
